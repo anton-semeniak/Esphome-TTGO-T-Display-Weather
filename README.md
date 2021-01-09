4e1f7d0d19aa026e7ae6356e3e8fab0826ab7e93
root <deftdawg@gmail.com>
2021-01-08 23:59:27 -0500

    - st7789v has been merged into ESPHome -> remove it - Patch changes from ESPHome 1.17-dev into "image" and "display" - Various tweaks to ttgo weather yaml configuration - Add .gitignore file - Remove __pycache__ directories - Add Hassio and fork notice to README.md

--- README.md
+++ README.md
@@ -12,11 +12,24 @@ ESPHome configuration to use TTGO T-Display for show curent weather from Home As
 * left_button - left onboard button on TTGO T-Display, turn off screen 
 * right_button - right onboard button on TTGO T-Display, turn on screen
 
-# Usage
-* Change secrets.yaml file for WiFi, OTA and API credentials. 
-* Apply ESPhome configuration
-* Register new device in Home Assistant.
-* Change and include sensors to the Home Assistant.
+# Setup under Hass.io
+* Open a terminal using either Terminal or VSCode addons
+```sh
+cd /config/esphome
+git clone THIS_REPOSITORY_URI
+cp -r Esphome-TTGO-T-Display-Weather/fonts .
+cp -r Esphome-TTGO-T-Display-Weather/images .
+cp -r Esphome-TTGO-T-Display-Weather/custom_components/ .
+echo '<<: !include Esphome-TTGO-T-Display-Weather/ttgo_weather.yaml' > ttgo_tdisplay_weather.yaml
+```
+* edit `Esphome-TTGO-T-Display-Weather/secrets.yaml` using `nano` (terminal) or `code` (in a vscode shell)
+* Within the ESPHome addon UI compile ttgo_tdisplay_weather and flash it onto your TTGO T-Display using [esphomeflasher](https://github.com/esphome/esphome-flasher)
+* If wifi is configured correctly, the device should appear as discovered in the `Configuration -> Integrations` screen
 
+# Usage
+* ~~Change secrets.yaml file for WiFi, OTA and API credentials.~~
+* ~~Apply ESPhome configuration~~
+* ~~Register new device in Home Assistant.~~
+* ~~Change and include sensors to the Home Assistant.~~
 
 Display custom component based on [musk95](https://github.com/musk95/esphome)
--- custom_components/image/__init__.py
+++ custom_components/image/__init__.py
@@ -1,16 +1,25 @@
-# coding=utf-8
 import logging
 
 from esphome import core
 from esphome.components import display, font
 import esphome.config_validation as cv
 import esphome.codegen as cg
-from esphome.const import CONF_FILE, CONF_ID, CONF_RESIZE, CONF_TYPE
+from esphome.const import CONF_FILE, CONF_ID, CONF_TYPE, CONF_RESIZE#, CONF_DITHER # CONF_DITHER is not available until 1.17
 from esphome.core import CORE, HexInt
+CONF_DITHER = 'dither'
+
 _LOGGER = logging.getLogger(__name__)
 
 DEPENDENCIES = ['display']
 MULTI_CONF = True
+
+ImageType = display.display_ns.enum('ImageType')
+IMAGE_TYPE = {
+    'BINARY': ImageType.IMAGE_TYPE_BINARY,
+    'GRAYSCALE': ImageType.IMAGE_TYPE_GRAYSCALE,
+    'RGB24': ImageType.IMAGE_TYPE_RGB24,
+    'RGB565': ImageType.IMAGE_TYPE_RGB565,
+}
 
 Image_ = display.display_ns.class_('Image')
 
@@ -20,7 +29,8 @@ IMAGE_SCHEMA = cv.Schema({
     cv.Required(CONF_ID): cv.declare_id(Image_),
     cv.Required(CONF_FILE): cv.file_,
     cv.Optional(CONF_RESIZE): cv.dimensions,
-    cv.Optional(CONF_TYPE): cv.string,
+    cv.Optional(CONF_TYPE, default='BINARY'): cv.enum(IMAGE_TYPE, upper=True),
+    cv.Optional(CONF_DITHER, default='NONE'): cv.one_of("NONE", "FLOYDSTEINBERG", upper=True),
     cv.GenerateID(CONF_RAW_DATA_ID): cv.declare_id(cg.uint8),
 })
 
@@ -34,17 +44,46 @@ def to_code(config):
     try:
         image = Image.open(path)
     except Exception as e:
-        raise core.EsphomeError(u"Could not load image file {}: {}".format(path, e))
+        raise core.EsphomeError(f"Could not load image file {path}: {e}")
+
+    width, height = image.size
 
     if CONF_RESIZE in config:
         image.thumbnail(config[CONF_RESIZE])
-
-    if config[CONF_TYPE].startswith('RGB565'):
         width, height = image.size
+    else:
+        if width > 500 or height > 500:
+            _LOGGER.warning("The image you requested is very big. Please consider using"
+                            " the resize parameter.")
+
+    dither = Image.NONE if config[CONF_DITHER] == 'NONE' else Image.FLOYDSTEINBERG
+    if config[CONF_TYPE] == 'GRAYSCALE':
+        image = image.convert('L', dither=dither)
+        pixels = list(image.getdata())
+        data = [0 for _ in range(height * width)]
+        pos = 0
+        for pix in pixels:
+            data[pos] = pix
+            pos += 1
+
+    elif config[CONF_TYPE] == 'RGB24':
+        image = image.convert('RGB')
+        pixels = list(image.getdata())
+        data = [0 for _ in range(height * width * 3)]
+        pos = 0
+        for pix in pixels:
+            data[pos] = pix[0]
+            pos += 1
+            data[pos] = pix[1]
+            pos += 1
+            data[pos] = pix[2]
+            pos += 1
+
+    elif config[CONF_TYPE] == 'RGB565':
         image = image.convert('RGB')
         pixels = list(image.getdata())
         data = [0 for _ in range(height * width * 2)]
-     
+
         pos = 0
         for pix in pixels:
             r = (pix[0] >> 3) & 0x1F
@@ -55,15 +94,9 @@ def to_code(config):
             pos += 1
             data[pos] = p & 0xFF
             pos += 1
-        rhs = [HexInt(x) for x in data]
-        prog_arr = cg.progmem_array(config[CONF_RAW_DATA_ID], rhs)
-        cg.new_Pvariable(config[CONF_ID], prog_arr, width, height, 1)
-    else:
-        image = image.convert('1', dither=Image.NONE)
-        width, height = image.size
-        if width > 500 or height > 500:
-            _LOGGER.warning("The image you requested is very big. Please consider using the resize "
-                            "parameter")
+
+    elif config[CONF_TYPE] == 'BINARY':
+        image = image.convert('1', dither=dither)
         width8 = ((width + 7) // 8) * 8
         data = [0 for _ in range(height * width8 // 8)]
         for y in range(height):
@@ -73,6 +106,7 @@ def to_code(config):
                 pos = x + y * width8
                 data[pos // 8] |= 0x80 >> (pos % 8)
 
-        rhs = [HexInt(x) for x in data]
-        prog_arr = cg.progmem_array(config[CONF_RAW_DATA_ID], rhs)
-        cg.new_Pvariable(config[CONF_ID], prog_arr, width, height)
+    rhs = [HexInt(x) for x in data]
+    prog_arr = cg.progmem_array(config[CONF_RAW_DATA_ID], rhs)
+    cg.new_Pvariable(config[CONF_ID], prog_arr, width, height,
+                     IMAGE_TYPE[config[CONF_TYPE]])
